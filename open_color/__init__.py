af4ce15acee7dc87566349d8501ca00793003824
Kevin Cao <kcao1998@gmail.com>
2019-10-05 19:08:52 -0400

    Finished.

---
+++ open_color/__init__.py
@@ -0,0 +1,176 @@
+'''
+
+Adapted from the open-source color library at https://yeun.github.io/open-color/
+
+'''
+white = (255, 255, 255)
+black = (0, 0, 0)
+
+# Gray
+gray_0 = (248, 249, 250)
+gray_1 = (241, 243, 245)
+gray_2 = (233, 236, 239)
+gray_3 = (222, 226, 230)
+gray_4 = (206, 212, 218)
+gray_5 = (173, 181, 189)
+gray_6 = (134, 142, 150)
+gray_7 = (73, 80, 87)
+gray_8 = (52, 58, 64)
+gray_9 = (33, 37, 41)
+grays = [gray_0,gray_1,gray_2,gray_3,gray_4,gray_5,gray_6,gray_7,gray_8,gray_9]
+
+# Red
+red_0 = (255, 245, 245)
+red_1 = (255, 227, 227)
+red_2 = (255, 201, 201)
+red_3 = (255, 168, 168)
+red_4 = (255, 135, 135)
+red_5 = (255, 107, 107)
+red_6 = (250, 82, 82)
+red_7 = (240, 62, 62)
+red_8 = (224, 49, 49)
+red_9 = (201, 42, 42)
+reds = [red_0,red_1,red_2,red_3,red_4,red_5,red_6,red_7,red_8,red_9]
+
+# Pink
+pink_0 = (255, 240, 246)
+pink_1 = (255, 222, 235)
+pink_2 = (252, 194, 215)
+pink_3 = (250, 162, 193)
+pink_4 = (247, 131, 172)
+pink_5 = (240, 101, 149)
+pink_6 = (230, 73, 128)
+pink_7 = (214, 51, 108)
+pink_8 = (194, 37, 92)
+pink_9 = (166, 30, 77)
+pinks = [pink_0,pink_1,pink_2,pink_3,pink_4,pink_5,pink_6,pink_7,pink_8,pink_9]
+
+# Grape
+grape_0 = (248, 240, 252)
+grape_1 = (243, 217, 250)
+grape_2 = (238, 190, 250)
+grape_3 = (229, 153, 247)
+grape_4 = (218, 119, 242)
+grape_5 = (204, 93, 232)
+grape_6 = (190, 75, 219)
+grape_7 = (174, 62, 201)
+grape_8 = (156, 54, 181)
+grape_9 = (134, 46, 156)
+grapes = [grape_0,grape_1,grape_2,grape_3,grape_4,grape_5,grape_6,grape_7,grape_8,grape_9]
+
+# Violet
+violet_0 = (243, 240, 255)
+violet_1 = (229, 219, 255)
+violet_2 = (208, 191, 255)
+violet_3 = (177, 151, 252)
+violet_4 = (151, 117, 250)
+violet_5 = (132, 94, 247)
+violet_6 = (121, 80, 242)
+violet_7 = (112, 72, 232)
+violet_8 = (103, 65, 217)
+violet_9 = (95, 61, 196)
+violets = [violet_0,violet_1,violet_2,violet_3,violet_4,violet_5,violet_6,violet_7,violet_8,violet_9]
+
+# Indigo
+indigo_0 = (237, 242, 255)
+indigo_1 = (219, 228, 255)
+indigo_2 = (186, 200, 255)
+indigo_3 = (145, 167, 255)
+indigo_4 = (116, 143, 252)
+indigo_5 = (92, 124, 250)
+indigo_6 = (76, 110, 245)
+indigo_7 = (66, 99, 235)
+indigo_8 = (59, 91, 219)
+indigo_9 = (54, 79, 199)
+indigos = [indigo_0,indigo_1,indigo_2,indigo_3,indigo_4,indigo_5,indigo_6,indigo_7,indigo_8,indigo_9]
+
+# Blue
+blue_0 = (231, 245, 255)
+blue_1 = (208, 235, 255)
+blue_2 = (165, 216, 255)
+blue_3 = (116, 192, 252)
+blue_4 = (77, 171, 247)
+blue_5 = (51, 154, 240)
+blue_6 = (34, 139, 230)
+blue_7 = (28, 126, 214)
+blue_8 = (25, 113, 194)
+blue_9 = (24, 100, 171)
+blues = [blue_0,blue_1,blue_2,blue_3,blue_4,blue_5,blue_6,blue_7,blue_8,blue_9]
+
+# Cyan
+cyan_0 = (227, 250, 252)
+cyan_1 = (197, 246, 250)
+cyan_2 = (153, 233, 242)
+cyan_3 = (102, 217, 232)
+cyan_4 = (59, 201, 219)
+cyan_5 = (34, 184, 207)
+cyan_6 = (21, 170, 191)
+cyan_7 = (16, 152, 173)
+cyan_8 = (12, 133, 153)
+cyan_9 = (11, 114, 133)
+cyans = [cyan_0,cyan_1,cyan_2,cyan_3,cyan_4,cyan_5,cyan_6,cyan_7,cyan_8,cyan_9]
+
+# Teal
+teal_0 = (230, 252, 245)
+teal_1 = (195, 250, 232)
+teal_2 = (150, 242, 215)
+teal_3 = (99, 230, 190)
+teal_4 = (56, 217, 169)
+teal_5 = (32, 201, 151)
+teal_6 = (18, 184, 134)
+teal_7 = (12, 166, 120)
+teal_8 = (9, 146, 104)
+teal_9 = (8, 127, 91)
+teals = [teal_0,teal_1,teal_2,teal_3,teal_4,teal_5,teal_6,teal_7,teal_8,teal_9]
+
+# Green
+green_0 = (235, 251, 238)
+green_1 = (211, 249, 216)
+green_2 = (178, 242, 187)
+green_3 = (140, 233, 154)
+green_4 = (105, 219, 124)
+green_5 = (81, 207, 102)
+green_6 = (64, 192, 87)
+green_7 = (55, 178, 77)
+green_8 = (47, 158, 68)
+green_9 = (43, 138, 62)
+greens = [green_0,green_1,green_2,green_3,green_4,green_5,green_6,green_7,green_8,green_9]
+
+# Lime
+lime_0 = (244, 252, 227)
+lime_1 = (233, 250, 200)
+lime_2 = (216, 245, 162)
+lime_3 = (192, 235, 117)
+lime_4 = (169, 227, 75)
+lime_5 = (148, 216, 45)
+lime_6 = (130, 201, 30)
+lime_7 = (116, 184, 22)
+lime_8 = (102, 168, 15)
+lime_9 = (92, 148, 13)
+limes = [lime_0,lime_1,lime_2,lime_3,lime_4,lime_5,lime_6,lime_7,lime_8,lime_9]
+
+# Yellow
+yellow_0 = (255, 249, 219)
+yellow_1 = (255, 243, 191)
+yellow_2 = (255, 236, 153)
+yellow_3 = (255, 224, 102)
+yellow_4 = (255, 212, 59)
+yellow_5 = (252, 196, 25)
+yellow_6 = (250, 176, 5)
+yellow_7 = (245, 159, 0)
+yellow_8 = (240, 140, 0)
+yellow_9 = (230, 119, 0)
+yellows = [yellow_0,yellow_1,yellow_2,yellow_3,yellow_4,yellow_5,yellow_6,yellow_7,yellow_8,yellow_9]
+
+# Orange
+orange_0 = (255, 244, 230)
+orange_1 = (255, 232, 204)
+orange_2 = (255, 216, 168)
+orange_3 = (255, 192, 120)
+orange_4 = (255, 169, 77)
+orange_5 = (255, 146, 43)
+orange_6 = (253, 126, 20)
+orange_7 = (247, 103, 7)
+orange_8 = (232, 89, 12)
+orange_9 = (217, 72, 15)
+oranges = [orange_0,orange_1,orange_2,orange_3,orange_4,orange_5,orange_6,orange_7,orange_8,orange_9]
